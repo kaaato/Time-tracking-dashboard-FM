0e0d535d88f1a5e3bfd0345e2e21d30aa0424e67
kaaato <kaaato@gmail.com>
2025-04-03 22:16:24 +0200

    add function updateText on main.js and <span class=text-tenses>s on index.html, to show different texts when clicking the tabs

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let tabsDiv = document.querySelector(".tabs");
 let buttons = document.querySelectorAll(".tab");
 let currentHrs = document.querySelectorAll(".current-hrs");
 let previousHrs = document.querySelectorAll(".previous-hrs");
+let descriptionSpans = document.querySelectorAll(".text-tenses");
 let url = "./data.json";
 
 tabsDiv.addEventListener("click", handleClick);
@@ -14,10 +15,10 @@ function handleClick(event) {
 
   toggleButtonHighlighted(target, buttons);
 
-  updateHrs(target, currentHrs, previousHrs);
+  updateHrs(target, currentHrs, previousHrs, descriptionSpans);
 }
 
-async function updateHrs(target, currentHrs, previousHrs) {
+async function updateHrs(target, currentHrs, previousHrs, spans) {
   try {
     let data = await fetchHrs(url);
 
@@ -25,6 +26,7 @@ async function updateHrs(target, currentHrs, previousHrs) {
     data.forEach((element, i) => {
       currentHrs[i].textContent = element.timeframes[title].current;
       previousHrs[i].textContent = element.timeframes[title].previous;
+      updateText(title, spans[i]);
     });
     return data;
   } catch(error) {
@@ -40,6 +42,16 @@ async function fetchHrs(url) {
     return data;
   } catch(error) {
     alert(error);
+  }
+}
+
+function updateText(title, span) {
+  if (title == "daily") {
+    span.textContent = "Yesterday - "
+  } else if (title == "weekly") {
+    span.textContent = "Last Week - ";
+  } else if (title == "monthly") {
+    span.textContent = "Last Month - "
   }
 }
 
